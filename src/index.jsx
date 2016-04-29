cf849b6b6b9ebaf092586fbbe54c176350daa2e1
kcvan <kcongvan@gmail.com>
2016-04-29 10:19:19 -0700

    Export SearchBar, render the SearchBar component, and add comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,16 @@
-// This imports react from the module. This knows how to render and nest components
+// This imports react from the module. This knows how to render and nest components. 
+// With libraries that we import, we don't need to specify a file path, unlike
+// files we create.
 import React from "react";
 // This library is how we actually render components to the DOM
 import ReactDOM from "react-dom";
-
+// Here we're importing out SearchBar function from search_bar.jsx.
+// SearchBar is just a variable/place holder, we can call it anything once we import it.
+// Unlike libraries, we have to specify the file path. You don't need to add an
+// extension if the file is a .js or .jsx file
+import SearchBar from "./components/search_bar";
 // Declaring a constant to hold our API key
+// Go to Google Youtube API to get your own API key
 const API_KEY = "";
 
 // 1) Create a new component. This component should produce some HTML.
@@ -13,14 +20,23 @@ const API_KEY = "";
 // You can think of this function as a factory that produces instances
 // of what is inside it. In this case, it's the div. To create the 
 // instance of App, it'll look like <App />
-// The fat arrow is new ES6 syntax, themain difference is that the "this"
+// The fat arrow is new ES6 syntax, the main difference is that the "this"
 // is pointed as different things.  
 const App = () => {
-  // Although this looks like HTML, this is still javascript using JSX, 
-  // a subset (or dialect) of JavaScript. Babel will transpile this into 
+  // Although this looks like HTML, this is still javascript using JSX which, 
+  // is a subset (or dialect) of JavaScript. Babel will transpile this into 
   // normal JavaScript that the browser can read. The purpose of JSX is to 
   // write JavaScript and have it rendered as HTML after being compiled.
-  return <div>Hi!</div>;
+  // For multiple components being rendered, it is a good convention to wrap
+  // your enclosing div with parathesis. React won't be able to render multiple
+  // components on their own, they will have to be enclosed in an html attribute.
+  // You can start to notice this tree-like structure that's forming, very similar 
+  // to actual HTML!
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 
