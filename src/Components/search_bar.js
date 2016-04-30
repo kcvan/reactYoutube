93fc5242dd1caa935db01e130cb92ae8cd82c819
kcvan <kcongvan@gmail.com>
2016-04-29 20:13:31 -0700

    Refactor to listen on a event change and to use arrow functions. Also add notes

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -27,18 +27,45 @@ import React, { Component } from "react";
 // components must have a render method. We changed the SearchBar from a function
 // component to a class-based component because we want it to pass information
 // along to other places, this is called a smart component, rather than a dumb
-// component that just renders jsx
+// component that just renders jsx.
 
 class SearchBar extends Component{
   // Although it does not look like a method with the classic key/value pair,
-  // it is indeed a function.
+  // it is indeed a function. The onChange is tracking for changes in the 
+  // input box. To track any type of event, you just type "on" + event={handler}.
+  // This reads as "Create a new input element and pass it a prop, onChange with
+  // the value of onInputChange".
   render() {
-    return <input />;
+    // return <input onChange={this.onInputChange} />;
+    // Refactor to use arrow functions
+    return <input onChange={event => console.log(event.target.value)} />;
   }
+
+  // Here, we declare an event handler which is a funtion that will run whenever
+  // the event occurs. We pass the handler to the element that we want to monitor
+  // the event. In our case, we want to keep track of whenever the input changes 
+  // Usually the syntax for event handlers will be "on" or "handle" + element + event.
+  // All native browser events will always get passed an event object, which stores
+  // information on the event that occured. Basically here, we can get the text that
+  // was changed from the event object.
+
+
+  // Refactored to use arrow function
+  // onInputChange(event) {
+  //   // You can see the changing values in the console with this \/
+  //   // console.log(event.target.value);
+  // }
+
+
+
 }
 
 // Exporting SearchBar will let us use SearchBar in other files, we'll need to 
 // import SearchBar on the other files in order to have access to the function.
 // default means that if we don't specify a function to export out of search_bar.jsx,
 // it will default to SearchBar
-export default SearchBar
+export default SearchBar;
+
+
+
+
