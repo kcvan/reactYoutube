590db8fc1ec5b073ed7bbe2d999d902c0a9102d3
kcvan <kcongvan@gmail.com>
2016-05-01 14:10:19 -0700

    Add video detail component with styling and markup

---
+++ src/Components/video_detail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// By now, we should be able to follow along with our code
+
+const VideoDetail = ({video}) => {
+
+  const videoId = video.id.videoId;
+  // This is called string interpolation, using backticks, we can put everything
+  // inside the backticks and it'll be a string. To add js variables, use ${}.
+  const url = `https://www.youtube.com/embed/${videoId}`;
+  return (
+    <div className="video-detail col-md-8">
+      <div className="embed-responsive embed-responsive-16by9"
+        <iframe className="embed-responsive-item" src={videoId}></iframe>
+      </div>
+      <div className="details">
+        <div>{video.snippet.title}</div>
+        <div>{video.snippet.description}</div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetail;
