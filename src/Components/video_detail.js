fb823f1170c0027339d58cb1ae3bc530ba2c868b
kcvan <kcongvan@gmail.com>
2016-05-01 14:35:21 -0700

    Fix typo and missing carrots

--- src/Components/video_detail.js
+++ src/Components/video_detail.js
@@ -3,6 +3,10 @@ import React from "react";
 // By now, we should be able to follow along with our code
 
 const VideoDetail = ({video}) => {
+  if(!video) {
+    return <div>Loading...</div>;
+  }
+
 
   const videoId = video.id.videoId;
   // This is called string interpolation, using backticks, we can put everything
@@ -10,8 +14,8 @@ const VideoDetail = ({video}) => {
   const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-responsive embed-responsive-16by9"
-        <iframe className="embed-responsive-item" src={videoId}></iframe>
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
