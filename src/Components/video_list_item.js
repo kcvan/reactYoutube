80420497a40555b3c9c5567f9a3b7b2b9fb8f64c
kcvan <kcongvan@gmail.com>
2016-05-01 13:50:36 -0700

    Add functional component that styles the video properties we get back from our api call

---
+++ src/Components/video_list_item.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// This component just returns a <li> with something in it. as you can see,
+// we're using es6 destructuring to just get video out of props object.
+const VideoListItem = ({video}) => {
+  // This syntax on top in the parameter is the same as below
+  // const video = props.video;
+
+  // This gives us an image from youtube api
+  const imageUrl = video.snippet.thumbnails.default.url;
+  // We're spiffing up our <li> with some bootstrap
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageUrl}/>
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default VideoListItem;
